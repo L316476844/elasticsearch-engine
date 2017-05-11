df96e9054190b184187dff477dd0346f4fdaf782
lvbin <lvbin@oriental-finance.com>
2017-05-11 09:49:24 +0800

    first commit

---
+++ src/main/java/org/jon/lv/App.java
@@ -0,0 +1,13 @@
+package org.jon.lv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
